9e9f8b74718e7a9cd7d45b19144461a05c15e511
idur23 <60172585+idur23@users.noreply.github.com>
2022-08-24 17:41:07 +0700

    Update navbar.php

--- application/modules/Anak/views/navbar.php
+++ application/modules/Anak/views/navbar.php
@@ -1,10 +1,10 @@
-<header class="navbar navbar-dark sticky-top bg-primary ">
-  <a class="navbar-brand col-md-1 col-lg-4 me-0 px-1" href="#">Pendaftaran Paud dan Taman Kanak Kanak Kosgoro</a>
+<header class="navbar navbar-dark sticky-top" style="background-color: #FFC0CB">
+  <a class="navbar-brand col-md-1 col-lg-5 me-0 px-1" href="#">Pendaftaran Paud dan Taman Kanak Kanak Kosgoro</a>
 
   
   <ul class="navbar-nav px-3">
     <li class="nav-item text-nowrap">
-      <a class="nav-link btn btn-dangger" href="<?php echo base_url('Utama'); ?>">Exit</a>
+      <a class="nav-link btn btn-dangger" style="color: black" href="<?php echo base_url('Utama'); ?>">Exit</a>
     </li>
   </ul>
 </header>
